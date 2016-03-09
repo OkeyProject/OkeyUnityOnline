c16ba17e59ac4cb98180039eb2e1dcd28aaba301
Justin <l6104400@gmail.com>
2016-03-09 20:00:54 +0800

    add card size const

---
+++ Assets/Script/Lib/CARD.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class CARD{
+	public static Vector3 HAND_SIZE {get{return new Vector3(0.3f, 0.05f, 0.45f);}}
+	public static Vector3 DECK_SIZE {get{return new Vector3(0.2f, 0.05f, 0.3f);}}
+
+}
---
+++ Assets/Script/Lib/POSITION.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class POSITION {
+	public static Vector3 HAND_POS {	
+		
+	}
+}
